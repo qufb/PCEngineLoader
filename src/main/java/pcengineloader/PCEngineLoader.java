b93837eff80de0212fd81d6d7babf884fff15e0d
qufb <qufb>
2022-12-27 16:34:42 +0000

    .

--- src/main/java/pcengineloader/PCEngineLoader.java
+++ src/main/java/pcengineloader/PCEngineLoader.java
@@ -29,6 +29,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import docking.widgets.OptionDialog;
 import ghidra.app.util.Option;
 import ghidra.app.util.OptionUtils;
 import ghidra.app.util.bin.BinaryReader;
@@ -58,8 +59,11 @@ import ghidra.util.task.TaskMonitor;
  */
 public class PCEngineLoader extends AbstractLibrarySupportLoader {
 
+	private static final int MAX_PAGES = 8;
 	private static final int MAX_BANKS = 0x80; // ROM size = 1MB
 
+	private boolean isROM = true;
+	
 	@Override
 	public String getName() {
 		return "PC-Engine / TurboGrafx-16";
@@ -90,32 +94,54 @@ public class PCEngineLoader extends AbstractLibrarySupportLoader {
 		//
 		// We assume that each vector is in MPR7's address range, which covers more cases
 		// with hopefully few false positives...
-		if (!is_pce) {
-			BinaryReader reader = new BinaryReader(provider, true);
-			is_pce = true;
-			for (int i = 0x1ff0; i < 0x1ff6; i += 2) {
-				int v = reader.readUnsignedShort(i);
-				if (v != 0xffff) {
-					is_pce = false;
-					break;
-				}
-			}
-			for (int i = 0x1ff6; i < 0x2000; i += 2) {
-				int v = reader.readUnsignedShort(i);
-				if (v < 0xe000 || v > 0xfff0) {
-					is_pce = false;
-					break;
-				}
-			}
+		is_pce |= findIntVecs(provider, 0x1FF0);
+		if (provider.length() == 0x10000) {
+			// Check paged offset on RAM dump image.
+			is_pce |= findIntVecs(provider, 0xFFF0);
 		}
 
 		if (is_pce) {
+			if (provider.length() == 0x10000) {
+				int choice =
+						OptionDialog.showOptionNoCancelDialog(
+						null,
+						"Choose image type",
+						"<html>Detected PC-Engine image with length=0x10000, is this a ROM or a RAM dump?<br>Pages are assigned automatically for RAM dumps.",
+						"ROM",
+						"RAM",
+					OptionDialog.QUESTION_MESSAGE);
+				if (choice == OptionDialog.OPTION_TWO) {
+					isROM = false;
+				}
+			}
+			
 			loadSpecs.add(new LoadSpec(this, 0, new LanguageCompilerSpecPair("HuC6280:LE:16:default", "default"), true));
 		}
 
 		return loadSpecs;
 	}
 
+	private boolean findIntVecs(ByteProvider provider, int offset) throws IOException {
+		boolean is_pce = true;
+		BinaryReader reader = new BinaryReader(provider, true);
+		
+		for (int i = offset; i < offset + 6; i += 2) {
+			int v = reader.readUnsignedShort(i);
+			if (v != 0xffff) {
+				is_pce = false;
+				break;
+			}
+		}
+		for (int i = offset + 6; i < offset + 0x10; i += 2) {
+			int v = reader.readUnsignedShort(i);
+			if (v < 0xe000 || v > 0xfff0) {
+				is_pce = false;
+				break;
+			}
+		}
+		
+		return is_pce;
+	}
 
 	@Override
 	protected void load(ByteProvider provider, LoadSpec loadSpec, List<Option> options,
@@ -131,22 +157,34 @@ public class PCEngineLoader extends AbstractLibrarySupportLoader {
 		 * page    7: bank $00
 		 */
 		final long bank_size = 0x2000L;
-		createSegment(fpa, null, "MPR0_IO",    0x0000, bank_size, true, true, false, true, false, log);
-		createSegment(fpa, null, "MPR1_ZPRAM", 0x2000, 0x100, true, true, false, true, false, log);
-		createSegment(fpa, null, "MPR1_STACK", 0x2100, 0x100, true, true, false, true, false, log);
-		createSegment(fpa, null, "MPR1_WRAM",  0x2200, 0x1e00, true, true, false, true, false, log);
-		createSegment(fpa, provider.getInputStream(0), "MPR7_ROM", (bank_size * 7), bank_size, true, false, true, false, false, log);
-
-		// Note that each ROM bank should be manually added to the program,
-		// with base address matching the page assigned in the user program (look for LDA + TAM instructions).
-		// Here we just overlay all banks in page 2.
 		final InputStream romStream = provider.getInputStream(0);
-		final long page2_offset = bank_size * 2;
-		for (int i = 0; i < MAX_BANKS; i++) {
-			createSegment(fpa, provider.getInputStream(Math.min(romStream.available(), bank_size * i)),
-					"Bank" + String.format("%02X", i), page2_offset, bank_size, true, true, true, false, true, log);
-			if (romStream.available() < bank_size * (i + 1)) {
-				break;
+		if (isROM) {
+			createSegment(fpa, null, "MPR0_IO",    0x0000, bank_size, true, true, false, true, false, log);
+			createSegment(fpa, null, "MPR1_ZPRAM", 0x2000, 0x100, true, true, false, true, false, log);
+			createSegment(fpa, null, "MPR1_STACK", 0x2100, 0x100, true, true, false, true, false, log);
+			createSegment(fpa, null, "MPR1_WRAM",  0x2200, 0x1e00, true, true, false, true, false, log);
+			createSegment(fpa, provider.getInputStream(0), "MPR7_ROM", (bank_size * 7), bank_size, true, false, true, false, false, log);
+
+			// Note that each ROM bank should be manually added to the program,
+			// with base address matching the page assigned in the user program (look for LDA + TAM instructions).
+			// Here we just overlay all banks in page 2.
+			final long page2_offset = bank_size * 2;
+			for (int i = 0; i < MAX_BANKS; i++) {
+				createSegment(fpa, provider.getInputStream(Math.min(romStream.available(), bank_size * i)),
+						"Bank" + String.format("%02X", i), page2_offset, bank_size, true, false, true, false, true, log);
+				if (romStream.available() < bank_size * (i + 1)) {
+					break;
+				}
+			}
+		} else {
+			createSegment(fpa, provider.getInputStream(0x0000), "MPR0_IO",    0x0000, bank_size, true, true, false, true, false, log);
+			createSegment(fpa, provider.getInputStream(0x2000), "MPR1_ZPRAM", 0x2000, 0x100, true, true, false, true, false, log);
+			createSegment(fpa, provider.getInputStream(0x2100), "MPR1_STACK", 0x2100, 0x100, true, true, false, true, false, log);
+			createSegment(fpa, provider.getInputStream(0x2200), "MPR1_WRAM",  0x2200, 0x1e00, true, true, false, true, false, log);
+			
+			for (int i = 2; i < MAX_PAGES; i++) {
+				createSegment(fpa, provider.getInputStream(bank_size * i),
+						"MPR" + i, bank_size * i, bank_size, true, false, true, false, false, log);
 			}
 		}
 
@@ -161,7 +199,7 @@ public class PCEngineLoader extends AbstractLibrarySupportLoader {
 				0x1FFEL, "Reset"
 				));
 		for (Map.Entry<Long, String> entry : intVecs.entrySet()) {
-			final long offset = entry.getKey();
+			final long offset = isROM ? entry.getKey() : entry.getKey() + 0xE000L;
 			final String name = entry.getValue();
 			reader.setPointerIndex(offset);
 			final Address address = fpa.toAddr(reader.readNextUnsignedShort());
@@ -175,6 +213,11 @@ public class PCEngineLoader extends AbstractLibrarySupportLoader {
 		List<AddressLabelInfo> labels = loadSpec.getLanguageCompilerSpec().getLanguage().getDefaultSymbols();
 		for (AddressLabelInfo info : labels) {
 			try {
+				// TODO: Add CD labels for CD images.
+				final long offset = info.getAddress().getUnsignedOffset();
+				if (offset >= 0x1800) {
+					continue;
+				}
 				program.getSymbolTable().createLabel(info.getAddress(), info.getLabel(), SourceType.IMPORTED);
 			} catch (InvalidInputException e) {
 				log.appendException(e);
